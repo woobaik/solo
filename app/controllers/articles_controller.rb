77e1688bb0058bcdf43e3c813907851af6003546
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-02-01 23:43:23 -0800

    Created edit, and show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,13 +10,31 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_param)
-    @article.save
 
     if @article.save
       flash[:notice] = "Article is successfully saved"
       redirect_to articles_path
     else
       render new_article_path
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_param)
+      flash[:notice] = "Article is successfully editted"
+      redirect_to articles_path(@article)
+    else
+      render edit_article_path
     end
   end
 
