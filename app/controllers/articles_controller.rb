42f5de0552748c8597d82a9cb3447c5e49564a7b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-02-01 18:55:49 -0800

    Modified table for 'index.html.erb', via 'articles_controller.rb'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-
+    @articles = Article.all
   end
 
   def new
@@ -9,6 +9,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    
+
   end
 end
