227bc3a43c7f01540256c11e9e4af75d1f6c84df
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-02-01 23:14:04 -0800

    Created new form for Articles, implemented error msg, and flash msg

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,21 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_param)
+    @article.save
+
+    if @article.save
+      flash[:notice] = "Article is successfully saved"
+      redirect_to articles_path
+    else
+      render new_article_path
+    end
   end
 
-  
+
+
+  private
+
+  def article_param
+    params.require(:article).permit(:title, :description)
+  end
 end
